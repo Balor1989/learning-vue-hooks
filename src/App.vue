cf985c9547807724755177706717ddc5977b4161
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-14 20:43:49 +0300

    use setup (composition API)

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,52 @@
   <div class="container">
     <div class="card">
       <h1>Vue Composition Api</h1>
-      <hr>
-      <p>Название: <strong>{{ name }}</strong></p>
-      <p>Версия: <strong>{{ version }}</strong></p>
+      <hr />
+      <p>
+        Название: <strong>{{ name }}</strong>
+      </p>
+      <p>
+        Версия: <strong>{{ version }}</strong>
+      </p>
+      <p>{{ newVersion }}</p>
 
-      <button class="btn" @click="changeInfo">Изменить</button>
+      <button class="btn" @click="change">Изменить</button>
     </div>
   </div>
 </template>
 
 <script>
+import { computed, reactive, ref } from "vue";
 export default {
-  data() {
+  setup() {
+    const name = ref("VueJS");
+    const version = ref(2);
+
+    // function changeInfo() {
+    //   name.value = "Vue JS!";
+    //   version.value = 3;
+    // }
+    const allVars = reactive({
+      name: "VueJS",
+      version: 2,
+    });
+
+    function changeInfo() {
+      name.value = "Vue JS!";
+      version.value = 3;
+    }
+
+    const newVersion = computed(() => {
+      return version.value * 3;
+    });
+
     return {
-      name: 'VueJS',
-      version: 3
-    }
+      name,
+      version,
+      change: changeInfo,
+      allVars,
+      newVersion,
+    };
   },
-  methods: {
-    changeInfo() {
-      this.name = 'Vue JS!'
-      this.version = 4
-    }
-  }
-}
+};
 </script>
